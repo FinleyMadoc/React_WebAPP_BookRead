3a6e35d4a66cf29d2a8623ed03113d919df52099
FinleyMadoc <786297455@qq.com>
2024-01-16 17:23:48 +0800

    补交修改
    1.16

--- react-book/src/base/space/index.tsx
+++ react-book/src/base/space/index.tsx
@@ -10,6 +10,27 @@ export interface SpaceProps {
     align?: 'start' | 'end' | 'center' | 'baseline';
     // 主轴对齐方向
     justify?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly' | 'stretch';
-    // 是否自动换行
-    wrap?: boolean
+    // 是否自动换行, 仅在horizontal的时候有效
+    wrap?: boolean;
+    // 是否渲染为块级元素
+    block?: boolean;
+    // 间距大小
+    gap?: number | string | [number | string, number | string];
+    // 元素点击事件
+    onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+    children?: React.ReactNode;
 }
+
+const classPrefix = `ygm-space`;
+
+const Space: React.FC<SpaceProps> = (props) => {
+    return <div></div>
+}
+
+Space.defaultProps = {
+    direction: 'horizontal',
+    block: true,
+}
+
+Space.displayName = 'Space';
+export default Space;
