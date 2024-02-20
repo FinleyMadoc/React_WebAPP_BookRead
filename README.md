ac084ef27cc080f4c44d8afc97e36be2d956ba9d
FinleyMadoc <786297455@qq.com>
2024-02-20 15:06:06 +0800

    提交说明
    提交readme

---
+++ README.md
@@ -0,0 +1,38 @@
+# 简介
+
+模拟书城项目
+前端： React18 + Vite + TS
+服务端： NodeJS
+
+## 练习
+
+- 抽象和封装组件库，上传npm( madoc-react-mobileui )
+- 练习自定义hooks，适应开发需求
+
+### 支持功能
+
+- 搜索书籍(保存记录、热门搜索)
+- 阅读书籍(目录、样式设置、进度条)
+- 书籍类别(分类、排行)
+- 书架编辑(分组、删除)
+
+### 截图
+
+- 首页
+![](screenshot/HomePage.jpg)
+
+- 分类
+![](screenshot/Rank.jpg)
+![](screenshot/Category.jpg)
+
+- 搜索
+![](screenshot/Search.jpg)
+
+- 书籍分组
+![](screenshot/Group.jpg)
+
+- 详情和阅读器
+![](screenshot/BookDetail.jpg)
+![](screenshot/BookDetail_Directory.jpg)
+![](screenshot/Reader.jpg)
+![](screenshot/Schedule.jpg)
--- react-book/src/base/card/index.tsx
+++ react-book/src/base/card/index.tsx
@@ -1,66 +1,67 @@
 import React from 'react';
 import cx from 'classnames';
 
-import './styles/index.module.scss';
+import './styles/index.scss';
 
 export interface CardProps {
-  // 左边
   title?: React.ReactNode;
-  // 右边
   extra?: React.ReactNode;
-  // 自定义类名
   headerClassName?: string;
-  // 左边标题的自定义类名
   titleClassName?: string;
-  // 右边更多内容的自定义类名
   extraClassName?: string;
-  bodyClassNmae?: string;
+  bodyClassName?: string;
   children?: React.ReactNode;
-  onHeaderClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-  onBodyClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onHeaderClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onBodyClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-const classPrefix = 'ygm-card';
-
-
-
-const Card: React.FC<CardProps> = (props) => {
-  const renderHeader = () => {
-    if (!(props.title || props.extra)) return null;
-    // console.log('props', props);
+const Card: React.FC<CardProps> = React.memo((props) => {
+  const renderHeader = React.useCallback(() => {
+    if (!(props.title || props.extra)) {
+      return null;
+    }
 
     return (
-      <div className={cx(`ygm-card-header`, props.headerClassName)} onClick={props.onHeaderClick}>
-          <div className={cx(`ygm-card-header-title`, props.titleClassName)}>{props.title}</div>
-          <div className={cx(`ygm-card-header-extra`, props.extraClassName)}>{props.extra}</div>
+      <div className={cx('ygm-card-header', props.headerClassName)} onClick={props.onHeaderClick}>
+        <div className={cx('ygm-card-header-title', props.titleClassName)}>{props.title}</div>
+        <div className={cx('ygm-card-header-extra', props.extraClassName)}>{props.extra}</div>
       </div>
-    //   <div style={{ display: 'flex', justifyContent: 'space-between' }} onClick={props.onHeaderClick}>
-    //     <div >{props.title}</div>
-    //     <div >{props.extra}</div>
-    //   </div>
     );
-  };
+  }, [
+    props.extra,
+    props.extraClassName,
+    props.headerClassName,
+    props.title,
+    props.titleClassName,
+    props.onHeaderClick,
+  ]);
 
-  const renderBody = () => {
-    if (!props.children) return null;
+  const renderBody = React.useCallback(() => {
+    if (!props.children) {
+      return null;
+    }
 
     return (
       <div
-        className={cx(`${classPrefix}-body`, props.bodyClassNmae)}
+        className={cx('ygm-card-body', props.bodyClassName)}
         onClick={props.onBodyClick}
-        style={{ paddingTop: props.title || props.extra ? 0 : 13 }}
+        style={{
+          paddingTop: props.title || props.extra ? 0 : 13,
+        }}
       >
         {props.children}
       </div>
     );
-  };
+  }, [props.bodyClassName, props.children, props.extra, props.onBodyClick, props.title]);
 
   return (
-    <div className={classPrefix}>
+    <div className="ygm-card">
       {renderHeader()}
       {renderBody()}
     </div>
   );
-};
+});
 
 export default Card;
+
+Card.displayName = 'Card';
